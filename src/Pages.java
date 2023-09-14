4c6d5f18caf8f0dd4742706abd942a61de4ca1b4
kylechan500 <144576753+kylechan500@users.noreply.github.com>
2023-09-14 16:33:57 +0800

    Update Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,15 @@
-public class Pages {
+import java.util.Scanner;
 
+public class Pages {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int UserAge = scanner.nextInt();
+        int DataAge = (100 - (UserAge));
+        System.out.println(UserAge + "-year olds should should read at least " + DataAge + " pages before giving up on a book.");
+
+
 
     }
 }
