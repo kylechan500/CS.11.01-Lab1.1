fc215c441c5436f7615e69b343ea4a618cfc9747
kylechan500 <144576753+kylechan500@users.noreply.github.com>
2023-09-14 16:29:19 +0800

    Update Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
-public class Dateable {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
+public class Dateable {
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+System.out.println("Please enter your age:");
+int UserAge = scanner.nextInt();
+int DataAge = (7 +(UserAge/2));
+System.out.println(UserAge + "-year olds should date someone who is at least " + DataAge + "-years old");
+
+
+
     }
 }
